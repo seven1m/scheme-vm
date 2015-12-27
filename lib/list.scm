ad0ce239dfd32d15c067eddade5a551ffb9f0d56
Tim Morgan <tim@timmorgan.org>
2015-12-26 19:52:11 -0600

    Add last function

--- lib/list.scm
+++ lib/list.scm
@@ -7,3 +7,11 @@
     (if (empty? l)
         0
         (+ 1 (length (cdr l))))))
+
+(define last
+  (lambda (l)
+    (if (empty? l)
+        (list)
+        (if (= 1 (length l))
+            (car l)
+            (last (cdr l))))))
---
+++ spec/lib/list-spec.scm
@@ -0,0 +1,4 @@
+(include "assert")
+
+(assert-eq 1 (last (list 1)))
+(assert-eq 3 (last (list 1 2 3)))
