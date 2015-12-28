264f21110ac1fc7fe7611eeadd7bee7f6920b05e
Tim Morgan <tim@timmorgan.org>
2015-12-28 11:52:09 -0600

    Replace and/or functions with macros

--- lib/logic.scm
+++ lib/logic.scm
@@ -1,22 +1,17 @@
-(define and
-  (lambda conditions
-    (if (= 0 (length conditions))
-        #t
-        (if (= 1 (length conditions))
-            (car conditions)
-            (if (car conditions)
-                (apply and (cdr conditions))
-                #f)))))
+(define-syntax and
+  (syntax-rules ()
+    ((and) #t)
+    ((and test) test)
+    ((and test1 test2 ...)
+     (if test1 (and test2 ...) #f))))
 
-(define or
-  (lambda conditions
-    (if (= 0 (length conditions))
-        #f
-        (if (= 1 (length conditions))
-            (car conditions)
-            (if (car conditions)
-                (car conditions)
-                (apply or (cdr conditions)))))))
+(define-syntax or
+  (syntax-rules ()
+    ((or) #f)
+    ((or test) test)
+    ((or test1 test2 ...)
+     (let ((x test1))
+       (if x x (or test2 ...))))))
 
 (define not
   (lambda (condition)
