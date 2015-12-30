20bd863fc4732d7b85688890e15a3ebea09827c3
Tim Morgan <tim@timmorgan.org>
2015-12-29 22:34:59 -0600

    Move list specs to scheme

--- spec/lib/list-spec.scm
+++ spec/lib/list-spec.scm
@@ -2,3 +2,9 @@
 
 (assert-eq 1 (last (list 1)))
 (assert-eq 3 (last (list 1 2 3)))
+
+(assert-eq #t (empty? (list)))
+(assert-eq #f (empty? (list 1 2 3)))
+
+(assert-eq 0 (length (list)))
+(assert-eq 3 (length (list 1 2 3)))
