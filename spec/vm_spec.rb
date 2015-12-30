0d40be85f658f8776f8972ae3a4fe5cab5703296
Tim Morgan <tim@timmorgan.org>
2015-12-30 16:15:16 -0600

    Move GC spec to its own file

--- spec/vm_spec.rb
+++ spec/vm_spec.rb
@@ -1042,69 +1042,6 @@ describe VM do
     end
   end
 
-  describe 'GC' do
-    let(:instructions) do
-      [
-        VM::PUSH_FUNC,
-
-        VM::PUSH_ARG,
-        VM::SET_LOCAL, 'my-2',
-
-        VM::PUSH_ARG,
-        VM::SET_LOCAL, 'my-3',
-
-        VM::PUSH_REMOTE, 'y',
-        VM::SET_LOCAL, 'my-y',
-
-        VM::HALT, # pause execution here
-        VM::RETURN,
-        VM::ENDF,
-        VM::SET_LOCAL, 'fn',
-
-        # # # # # #
-
-        VM::PUSH_NUM, '1',
-        VM::SET_LOCAL, 'y',
-
-        VM::PUSH_NUM, '2',
-        VM::DUP,                # save this for an arg below
-        VM::PUSH_NUM, 1,
-        VM::PUSH_LIST,
-        VM::SET_LOCAL, 'z',
-
-        VM::PUSH_NUM, '3',
-        VM::PUSH_NUM, 2,
-        VM::SET_ARGS,           # [2, 3]
-
-        VM::PUSH_LOCAL, 'fn',
-        VM::CALL,
-        VM::HALT
-      ]
-    end
-
-    before { subject.execute(instructions) }
-
-    it 'cleans up objects no longer referenced by variables' do
-      expect(subject.heap[instructions.size..-1]).to match_array([
-        VM::Int.new(1),          # y
-        VM::Int.new(2),          # part of list at top-level
-        VM::EmptyList.instance,
-        VM::Pair,                # list defined at top level
-        VM::Int.new(3),          # argument passed to function (not set at the top level)
-        nil
-      ])
-      subject.execute            # resume execution
-      expect(subject.heap[instructions.size..-1]).to match_array([
-        VM::Int.new(1),          # y
-        VM::Int.new(2),          # still part of list at top-level
-        VM::EmptyList.instance,
-        VM::Pair,                # list defined at top level
-        nil,                     # argument passed to function, now out of scope
-        nil
-      ])
-    end
-  end
-
   describe 'tail call elimination' do
     before do
       c = Compiler.new([
