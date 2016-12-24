cf142603cc889aac808fd569e838f57e42f085ad
Tim Morgan <tim@timmorgan.org>
2016-12-24 00:00:27 -0600

    Clarify unspecified/undefined spec

---
+++ spec/lib/unspecified-spec.scm
@@ -0,0 +1,9 @@
+(import (scheme base)
+        (assert))
+
+;; AFAICT R7RS does not specify a nil/void/undefined value,
+;; but Chicken Scheme has #<unspecified>, which we copied.
+
+(define unspecified (if #f #f))
+
+(assert (eq? unspecified (cond (#f #f))))
