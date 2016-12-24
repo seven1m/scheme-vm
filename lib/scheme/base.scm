ebeab8d084706391b32cc7862c2bd6ad5589d5ca
Tim Morgan <tim@timmorgan.org>
2016-12-24 15:26:04 -0600

    Add caar, cadr, ... cddddr functions

--- lib/scheme/base.scm
+++ lib/scheme/base.scm
@@ -16,6 +16,9 @@
    begin
    boolean?
    car
+   caar cadr cdar cddr
+   caaar caadr cadar caddr cdaar cdadr cddar cdddr
+   caaaar caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar cdaadr cdadar cdaddr cddaar cddadr cdddar cddddr
    case
    cdr
    char?
@@ -248,6 +251,37 @@
           (if (eqv? obj (car list))
               list
               (memq obj (cdr list)))))
+
+    (define (caar l) (car (car l)))
+    (define (cadr l) (car (cdr l)))
+    (define (cdar l) (cdr (car l)))
+    (define (cddr l) (cdr (cdr l)))
+
+    (define (caaar l) (car (car (car l))))
+    (define (caadr l) (car (car (cdr l))))
+    (define (cadar l) (car (cdr (car l))))
+    (define (caddr l) (car (cdr (cdr l))))
+    (define (cdaar l) (cdr (car (car l))))
+    (define (cdadr l) (cdr (car (cdr l))))
+    (define (cddar l) (cdr (cdr (car l))))
+    (define (cdddr l) (cdr (cdr (cdr l))))
+
+    (define (caaaar l) (car (car (car (car l)))))
+    (define (caaadr l) (car (car (car (cdr l)))))
+    (define (caadar l) (car (car (cdr (car l)))))
+    (define (caaddr l) (car (car (cdr (cdr l)))))
+    (define (cadaar l) (car (cdr (car (car l)))))
+    (define (cadadr l) (car (cdr (car (cdr l)))))
+    (define (caddar l) (car (cdr (cdr (car l)))))
+    (define (cadddr l) (car (cdr (cdr (cdr l)))))
+    (define (cdaaar l) (cdr (car (car (car l)))))
+    (define (cdaadr l) (cdr (car (car (cdr l)))))
+    (define (cdadar l) (cdr (car (cdr (car l)))))
+    (define (cdaddr l) (cdr (car (cdr (cdr l)))))
+    (define (cddaar l) (cdr (cdr (car (car l)))))
+    (define (cddadr l) (cdr (cdr (car (cdr l)))))
+    (define (cdddar l) (cdr (cdr (cdr (car l)))))
+    (define (cddddr l) (cdr (cdr (cdr (cdr l)))))
 
     (define-syntax case
       (syntax-rules (else =>)
--- spec/lib/pair-spec.scm
+++ spec/lib/pair-spec.scm
@@ -5,10 +5,17 @@
 (assert (not (pair? "string")))
 (assert (not (pair? '())))
 (assert (not (pair? (list))))
-(assert (pair? (list 1)))
+(assert (pair? '(1)))
 
-(assert (eq? 1 (car '(1 . 2))))
-(assert (eq? 1 (car (list 1 2))))
+(assert (= 1 (car '(1 . 2))))
+(assert (= 1 (car '(1 2))))
 
-(assert (eq? 2 (cdr '(1 . 2))))
-(assert (equal? (list 2) (cdr (list 1 2))))
+(assert (= 2 (cdr '(1 . 2))))
+(assert (equal? '(2) (cdr '(1 2))))
+
+(define nested-list '((1 2) (3 4) (5 6)))
+
+(assert (equal?        1 (caar nested-list)))
+(assert (equal?   '(3 4) (cadr nested-list)))
+(assert (equal?     '(2) (cdar nested-list)))
+(assert (equal? '((5 6)) (cddr nested-list)))
