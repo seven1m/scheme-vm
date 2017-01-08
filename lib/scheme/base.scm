140522ad557f13cfff6c591322035ddcf59787fc
Tim Morgan <tim@timmorgan.org>
2017-01-08 09:44:46 -0600

    Fix indentation

--- lib/scheme/base.scm
+++ lib/scheme/base.scm
@@ -229,12 +229,12 @@
         (if test1 (and test2 ...) #f))))
 
     (define-syntax or
-    (syntax-rules ()
-      ((or) #f)
-      ((or test) test)
-      ((or test1 test2 ...)
-        (let ((x test1))
-          (if x x (or test2 ...))))))
+      (syntax-rules ()
+        ((or) #f)
+        ((or test) test)
+        ((or test1 test2 ...)
+          (let ((x test1))
+            (if x x (or test2 ...))))))
 
     (define not
       (lambda (condition)
