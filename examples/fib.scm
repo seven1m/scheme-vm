964956375395c858bcd43e040eaa9532ab3fccc3
Tim Morgan <tim@timmorgan.org>
2017-01-16 19:20:45 -0600

    Add TCO version of fib example

--- examples/fib.scm
+++ examples/fib.scm
@@ -6,5 +6,15 @@
       (+
         (fib (- n 1))
         (fib (- n 2)))))
+
+(define (fib2 n)
+  (letrec ((f (lambda (n1 n2 c)
+                (if (= c n)
+                  n2
+                  (f n2 (+ n1 n2) (+ c 1))))))
+    (f 0 1 1)))
+
 (write-string (fib 8))
+(newline)
+(write-string (fib2 8))
 (newline)
