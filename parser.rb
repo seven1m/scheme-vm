54f0d1abf3ff83777c270c0a296a472aa88ae513
Tim Morgan <tim@timmorgan.org>
2018-02-24 18:47:17 -0600

    Load proper library file

--- parser.rb
+++ parser.rb
@@ -24,6 +24,14 @@ class Parser
   end
 end
 
-library = Fiddle::dlopen('target/release/libscheme_vm.dylib')
+LIB_PATHS = %w[
+  target/release/libscheme_vm.so
+  target/debug/libscheme_vm.so
+  target/release/libscheme_vm.dylib
+  target/debug/libscheme_vm.dylib
+]
+
+library_path = LIB_PATHS.detect { |path| File.exist?(path) }
+library = Fiddle::dlopen(library_path)
 init_parser = Fiddle::Function.new(library['init_parser'], [], Fiddle::TYPE_VOID)
 init_parser.call
